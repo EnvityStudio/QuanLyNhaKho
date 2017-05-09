23c576faa6e1704013c9a049a2edc4e865513752
minhhangmta <minhhang.mta@gmail.com>
2017-05-09 16:43:00 +0700

    InputKeyPress

--- QuanLyNhaKho/GUI/UCHangHoa.cs
+++ QuanLyNhaKho/GUI/UCHangHoa.cs
@@ -113,6 +113,17 @@ namespace QuanLyNhaKho.GUI
         }
         public void InputKeyPress(object sender, KeyPressEventArgs e)
         {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
         }
         public void AddHH()
         {
